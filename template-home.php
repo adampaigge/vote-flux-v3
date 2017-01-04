7ad340b5bca2d824ff190c07da0c9b889ba3bf79
Andrew Brown <ab@substrate.digital>
2017-01-04 20:29:30 +1100

    Add initial content and style accordingly

--- template-home.php
+++ template-home.php
@@ -6,8 +6,13 @@
 
 <?php while (have_posts()) : the_post(); ?>
 
-	<div class="homepageWrp">
-
+	<div class="homepageBg"></div>
+	<div class="homepageContent">
+		<div class="logoHomepage"></div>
+		<h1>Upgrade the system and return power to the people.</h1>
+		<h4>When did our politics become so broken? Things must change. Flux is your way to participate directly in parliament. Empowering people in government decisions directly through technology. Let’s break up the party politics, end the gridlock and return the power to where it belongs - you.</h4>
+		<div class="btnHex bgcOrng brdOrng"><a>Join Now</a></div>
+		<div class="btnHex bgcDark brdDark"><a>Learn More</a></div>
 	</div>
 
 <?php endwhile; ?>
--- templates/head.php
+++ templates/head.php
@@ -1,8 +1,9 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script type="text/javascript" src="//fast.fonts.net/jsapi/34a16449-cd63-4478-9b74-36678baaedc2.js"></script>
+	<?php wp_head(); ?>
 </head>
 
 <header class="banner">
